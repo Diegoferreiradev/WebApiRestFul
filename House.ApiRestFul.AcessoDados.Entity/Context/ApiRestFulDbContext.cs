b9de0c86216b7840cdb57e4f3a62a40dc15211a8
Diegoferreiradev <diegoferreiradev@gmail.com>
2019-01-28 01:23:13 -0300

    Desenvolvimento da Classe DbContext | ConnectionString e a Migration

---
+++ House.ApiRestFul.AcessoDados.Entity/Context/ApiRestFulDbContext.cs
@@ -0,0 +1,21 @@
+﻿using House.ApiRestFul.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace House.ApiRestFul.AcessoDados.Entity.Context
+{
+    public class ApiRestFulDbContext : DbContext
+    {
+        public DbSet<Aluno> Alunos { get; set; }
+
+        public ApiRestFulDbContext()
+        {
+            Configuration.ProxyCreationEnabled = false;
+            Configuration.LazyLoadingEnabled = false;
+        }
+    }
+}
